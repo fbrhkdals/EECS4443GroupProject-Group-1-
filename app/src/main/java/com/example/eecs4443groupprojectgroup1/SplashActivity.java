e90f61cddd1022f33a7de41a3feee5861ac33bb9
KwangminRyu <rkm9864@gmail.com>
2025-10-13 16:51:59 -0400

    Update SplashActivity.java

--- app/src/main/java/com/example/eecs4443groupprojectgroup1/SplashActivity.java
+++ app/src/main/java/com/example/eecs4443groupprojectgroup1/SplashActivity.java
@@ -14,15 +14,15 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash); // 레이아웃을 activity_splash로 설정
 
-        // 이미지 로딩 (로고 이미지)
+        // Load img
         ImageView logoImageView = findViewById(R.id.logo_image); // activity_splash.xml에서 ImageView의 ID 설정
 
-        // 2초 후에 MainActivity로 이동
+        // Move to MainActivity after 2sec
         new Handler().postDelayed(() -> {
             // MainActivity로 이동
             Intent intent = new Intent(SplashActivity.this, MainActivity.class);
             startActivity(intent);
-            finish(); // SplashActivity 종료
+            finish(); // SplashActivity End
         }, 2000); // 2000ms (2초)
     }
 }
